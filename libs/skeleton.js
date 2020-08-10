9e97af21366cc4ed0a403eee0dc5b01cc03cdb04
zhangyinxiong <zhangyinxiong@meituan.com>
2020-08-10 17:33:30 +0800

    commit

--- libs/skeleton.js
+++ libs/skeleton.js
@@ -1,44 +1,66 @@
+const $step = document.getElementById('step')
+const $type = document.getElementById('type')
+const $shimmer = document.getElementById('shimmer')
+const $colorControls = Array.from(document.querySelectorAll('.colors'))
 
-function rounded (ctx, x, y, w, h, r) {
-    if ( w < 2 * r) r = w / 2
-    if (h < 2 * r) r = h / 2
-    ctx.beginPath()
-    ctx.moveTo(x + r, y)
-    ctx.arcTo(x + w, y, x + w, y + h, r)
-    ctx.arcTo(x + w, y + h, x, y + h, r)
-    ctx.arcTo(x, y + h, x, y , r)
-    ctx.arcTo(x, y, x + w, y , r)
-    ctx.closePath()
-}
+let type = $type.value
+let step = parseFloat($step.value)
+let isShimmer = $shimmer.checked
+let colors = $colorControls.map(d => d.value || '#000000').concat($colorControls[0].value).map(d => toRGB(d))
+let offset = 0
+let delayFrame = 0
 
-let type = 'color-burn'
-
-document.getElementById('type').addEventListener('change', e => {
+$type.addEventListener('change', e => {
     type = e.target.value
 })
+$step.addEventListener('keydown', e => {
+    step = Number(e.target.value) || 20
+})
+$shimmer.addEventListener('change', e => {
+    isShimmer = e.target.checked
+})
+$colorControls.forEach((el, i) => {
+  el.addEventListener('input', e => {
+      colors[i] = toRGB(e.target.value)
+      if (i === 0) {
+          colors[colors.length - 1] = colors[0]
+      }
+  })
+})
+
+function toRGB (color) {
+    const s = color.substring(1, 7)
+    const r = []
+    for (let i = 0; i < s.length; i +=2){
+        r.push(parseInt(s.substr(i, 2), 16))
+    }
+    return r.join(',')
+}
 
 
-function skeleton (canvas) {
+function skeleton (canvas, data) {
     const ctx = canvas.getContext('2d')
-    console.log(data1)
-    const layers1 = data1.filter(d => d.shimmer !== true)
-    const layers2 = data1.filter(d => d.shimmer === true)
-    console.log(layers2)
+    const layers1 = data.filter(d => d.shimmer !== true)
+    const layers2 = data.filter(d => d.shimmer === true)
     const sc = shadowCanvas(canvas, layers2)
+    data = normalizeData(data)
 
     function render () {
+        delayFrame++
         ctx.save()
         ctx.fillStyle = '#f8f8f8'
         ctx.fillRect(0, 0, canvas.width, canvas.height)
-        layers1.forEach(d => {
+        const layers = isShimmer ? layers1 : data
+        layers.forEach((d, i) => {
             rounded(ctx, d.x, d.y, d.width, d.height, d.radius)
-            ctx.fillStyle = d.color
+            ctx.fillStyle = getRGBA(d, i)
             ctx.fill()
         })
 
-        sc.render()
-        ctx.drawImage(sc.canvas, 0, 0, canvas.width, canvas.height)
-
+        if (isShimmer) {
+            sc.render()
+            ctx.drawImage(sc.canvas, 0, 0, canvas.width, canvas.height)
+        }
         ctx.restore()
         requestAnimationFrame(render)
     }
@@ -50,14 +72,9 @@ function shadowCanvas (canvas, data) {
     const shadowCanvas = document.createElement('canvas')
     shadowCanvas.width = canvas.width
     shadowCanvas.height = canvas.height
-
     shadowCanvas.style.cssText = 'display:none'
-    document.body.appendChild(shadowCanvas)
 
     const ctx = shadowCanvas.getContext('2d')
-
-    let offset = 0
-    let step = 20
 
     function render () {
         ctx.save()
@@ -68,15 +85,18 @@ function shadowCanvas (canvas, data) {
             ctx.fill()
         })
 
-        ctx.globalCompositeOperation = 'source-in'
+        ctx.globalCompositeOperation = type
 
         if (offset > 1000) {
             offset = -1000
         }
         const gradient = ctx.createLinearGradient(offset, offset, offset + canvas.width, offset + canvas.height)
-        gradient.addColorStop(0, 'rgba(0,0,0,0.02)')
-        gradient.addColorStop(.5, 'rgba(0,0,0,0.1)')
-        gradient.addColorStop(1, 'rgba(0,0,0,0.02)')
+        gradient.addColorStop(0, `rgba(${colors[0]}, .01`)
+        for (let i = 1; i < colors.length; i++) {
+            const alpha = Math.sin(Math.PI / colors.length * i) * 0.05 + 0.05
+            gradient.addColorStop(parseFloat((i/colors.length).toFixed(1)), `rgba(${colors[i]}, ${alpha})`)
+        }
+        gradient.addColorStop(1, `rgba(${colors[colors.length - 1]}, .01`)
 
         ctx.fillStyle = gradient
         ctx.fillRect(0, 0, canvas.width, canvas.height)
@@ -89,4 +109,42 @@ function shadowCanvas (canvas, data) {
         canvas: shadowCanvas,
         render
     }
+}
+
+function normalizeData (data) {
+    return data.map((d, i) => {
+        if (d.color) {
+            d.rgb = toRGB(d.color)
+        }
+        if (d.color.length === 9) {
+            d.a = parseInt(d.color.substr(7, 2), 16) / 255
+        } else {
+            d.a = 1
+        }
+        d.alphaDelay = i * 2
+        d.alphaFrame = 100
+        return d
+    })
+}
+
+function getRGBA(layer, i) {
+    let a = delayFrame < layer.alphaDelay ? 0 : layer.a * (1 / layer.alphaFrame)
+    if (a < layer.a) {
+        layer.alphaFrame--
+    } else {
+        a = layer.a
+    }
+    return `rgba(${layer.rgb}, ${a})`
+}
+
+function rounded (ctx, x, y, w, h, r) {
+    if ( w < 2 * r) r = w / 2
+    if (h < 2 * r) r = h / 2
+    ctx.beginPath()
+    ctx.moveTo(x + r, y)
+    ctx.arcTo(x + w, y, x + w, y + h, r)
+    ctx.arcTo(x + w, y + h, x, y + h, r)
+    ctx.arcTo(x, y + h, x, y , r)
+    ctx.arcTo(x, y, x + w, y , r)
+    ctx.closePath()
 }
